24956540758d59cb6d8cbc65230a65b27b74dc1d
Adam Okoń <adam.okon@elegantcode.pl>
2017-12-09 13:39:20 +0100

    add generationg uuid for user

--- lib/stock_beam/user.ex
+++ lib/stock_beam/user.ex
@@ -2,11 +2,16 @@ defmodule StockBeam.User do
   @moduledoc """
   Module specified all user actions and logic in the app
   """
+  @uuid_base 3453234233
 
   @enfoce_keys [:name]
   defstruct [:name, :wallets, :uuid]
 
   def new(name) do
-    {:ok, %StockBeam.User{name: name, wallets: [], uuid: ""}}
+    {:ok, %StockBeam.User{name: name, wallets: [], uuid: generate_uuid()}}
+  end
+
+  defp generate_uuid do
+    Integer.to_string(:rand.uniform(@uuid_base), 32) <> Integer.to_string(:rand.uniform(@uuid_base), 32)
   end
 end
