6a271ef964e15d5b8c21f3f9ea151422157d40d2
Adam Okoń <adam.okon@elegantcode.pl>
2017-12-10 13:08:28 +0100

    split bids and asks to separate lists

--- lib/stock_beam/market.ex
+++ lib/stock_beam/market.ex
@@ -1,14 +1,18 @@
 defmodule StockBeam.Market do
   alias __MODULE__
 
-  @enforce_keys [:transactions]
-  defstruct [:transactions]
+  @enforce_keys [:bids, :asks]
+  defstruct [:bids, :asks]
 
   def new do
-    {:ok, %Market{transactions: []}}
+    {:ok, %Market{bids: [], asks: []}}
   end
 
-  def add_transaction(market, transaction) do
-    {:ok, %Market{transactions: [transaction | market.transactions]} }
+  def add_transaction(:bid, market, transaction) do
+    {:ok, %Market{market | bids: [transaction | market.bids]} }
+  end
+
+  def add_transaction(:ask, market, transaction) do
+    {:ok, %Market{market | asks: [transaction | market.asks]} }
   end
 end
